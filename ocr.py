8e0cb3b38f5d8c9f89a95ab55db217c912cc5413
Mhmmd Fahri <mhmmdfahri101204@gmail.com>
2026-05-06 00:52:51 +0700

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -5,12 +5,14 @@ pytesseract.pytesseract.tesseract_cmd = r"c:\Program Files\Tesseract-OCR\tessera
 def ocr_image(image_path):
     img = cv2.imread(image_path)
 
-    #cv2.imshow("weakltd", img)
-
+    #ini buat menampilan gambar weakltd.jpg yang saya buat 
+    cv2.imshow("weakltd", img)
+    cv2.waitKey(0) #ini buat menahan jendela nya agar tidak langsung tertutup
+    
+    #ini buat menyimpan data text yang ada di lrm.jpg
     text = pytesseract.image_to_string(img)
     return text
     
     
-
 hasil = ocr_image("img/lrm.jpg")
 print(hasil)
